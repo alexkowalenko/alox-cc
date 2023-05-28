adb5a051a34c0315a77c7469d1f640419dc180e9
Alex Kowalenko <alex.kowalenko@gmail.com>
2023-05-28 22:38:31 +1000

    Fix for headers for LLVM 16.0.4

--- src/context.hh
+++ src/context.hh
@@ -4,6 +4,8 @@
 
 #pragma once
 
+#include <array>
+
 #include "object.hh"
 #include "scanner.hh"
 
--- src/parser.hh
+++ src/parser.hh
@@ -4,6 +4,7 @@
 
 #pragma once
 
+#include <functional>
 #include <memory>
 #include <ostream>
 #include <string_view>
